61d8c063533d45f88c6424f85489f057fdfc1681
Ed Scheinerman <ers@jhu.edu>
2025-02-01 16:23:09 -0500

    First steps working

--- src/Matroids.jl
+++ src/Matroids.jl
@@ -5,7 +5,51 @@ using LinearAlgebra
 using LinearAlgebraX
 
 import LinearAlgebra: rank
+import Graphs: ne
+
+export Matroid, UniformMatroid, rank, ne
 
 include("RankFunctions.jl")
+
+struct Matroid
+    m::Int
+    r::AbstractRankFunction
+    function Matroid(mm::Int, rr::AbstractRankFunction)
+        return new(mm, rr)
+    end
+    function Matroid(A::Matrix)
+        _, m = size(A)
+        return new(m, MatrixRankFunction(A))
+    end
+end
+
+function UniformMatroid(m::Int, k::Int)
+    rf = UniformRankFunction(m, k)
+    return Matroid(m, rf)
+end
+
+"""
+    ne(M::Matroid)
+
+Return the number of elements in the matroid `M`.
+"""
+ne(M::Matroid) = M.m
+
+"""
+    rank(M::Matroid, S::Set{T}) where T<:Integer
+    rank(M::Matroid)
+
+Return the rank of the set `S` in the matroid `M`. 
+
+Without `S`, return the rank of the matroid `M`.
+"""
+function rank(M::Matroid, S::Set{T}) where {T<:Integer}
+    return M.r(S)
+end
+
+function rank(M::Matroid)
+    S = Set(1:ne(M))
+    return M.r(S)
+end
 
 end # module Matroids
--- src/RankFunctions.jl
+++ src/RankFunctions.jl
@@ -1,6 +1,6 @@
 abstract type AbstractRankFunction end
 
-export AbstractRankFunction, MatrixRankFunction
+# export AbstractRankFunction, MatrixRankFunction
 
 struct MatrixRankFunction <: AbstractRankFunction
     A::Matrix
@@ -9,8 +9,49 @@ struct MatrixRankFunction <: AbstractRankFunction
     end
 end
 
+"""
+    _column_picker(A::Matrix, S::Set{T}) where {T<:Integer}
+
+Return the submatrix of `A` using the columns specified in `S`.
+"""
 function _column_picker(A::Matrix, S::Set{T}) where {T<:Integer}
     return A[:, collect(S)]
 end
 
-(mr::MatrixRankFunction)(S::Set{T}) where {T<:Integer} = rankx(_column_picker(mr.A, S))
+"""
+    _set_check(S::Set{T}, m::Int) where {T<:Integer}
+
+Check `S` is a subset of `{1,2,...,m}`. If not, throw an error.
+"""
+function _set_check(S::Set{T}, m::Int) where {T<:Integer}
+    if isempty(S)
+        return nothing
+    end
+    if minimum(S) > 0 && maximum(S) <= m
+        return nothing
+    end
+    throw(DomainError(S, "Invalid subset of the matroid elements"))
+end
+
+function (mr::MatrixRankFunction)(S::Set{T}) where {T<:Integer}
+    _, m = size(mr.A)
+    _set_check(S, m)
+    return rankx(_column_picker(mr.A, S))
+end
+
+struct UniformRankFunction <: AbstractRankFunction
+    m::Int
+    k::Int
+    function UniformRankFunction(mm, kk)
+        if mm < 1 || kk < 0 || kk > mm
+            throw(DomainError((mm, kk), "Invalid arguments for UniformMatroid"))
+        end
+        return new(mm, kk)
+    end
+end
+
+function (ur::UniformRankFunction)(S::Set{T}) where T<:Integer
+    _set_check(S, ur.m)
+    ns = length(S)
+    return min(ur.k, ns)
+end
