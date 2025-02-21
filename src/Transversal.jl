0bdb0fb56942a07997085e84c4e974d68ea6132e
Ed Scheinerman <ers@jhu.edu>
2025-02-21 14:35:39 -0500

    Constructor work

--- src/Transversal.jl
+++ src/Transversal.jl
@@ -34,7 +34,26 @@ function (r::TransversalRankFunction)(X::Set)
     return -v
 end
 
+function _min_max_sets(the_sets::Vector{Set{T}}) where {T<:Integer}
+    if length(the_sets) == 0
+        return 1, 1
+    end
+    A = union(the_sets...)
+    return minimum(A), maximum(A)
+end
+
+"""
+    TransversalMatroid(m::Int, the_sets::Vector{Set{T}}) where {T<:Integer}
+
+Create a transversal matroid with ground set `S={1,2,...,m}`. Every set in the list 
+`the_sets` should be a subset of `S`.
+"""
 function TransversalMatroid(m::Int, the_sets::Vector{Set{T}}) where {T<:Integer}
+    a,b = _min_max_sets(the_sets)
+    if a < 0 || b > m 
+        throw(ArgumentError("Set must be all be subsets of {1,...,$m}"))
+    end
+
     r = TransversalRankFunction(m, the_sets)
     return Matroid(m, r)
 end
